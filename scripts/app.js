dca83b3fcd2f5826385b81fe4b8584a48b1fc86a
JorgeToAn <jorgetosta.f@gmail.com>
2022-07-12 16:47:56 -0700

    Assignment 1

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,11 @@
 //user constructor
 class User{
-    constructor(firstName, lastName, email, password, payment, color){
+    constructor(firstName, lastName, age, address, phone, email, password, payment, color){
         this.firstName=firstName;
         this.lastName=lastName;
+        this.age=age;
+        this.address=address;
+        this.phone=phone;
         this.email=email;
         this.password=password;
         this.payment=payment;
@@ -17,12 +20,15 @@ class User{
 function register(){
     let uFirstName=$("#txtFirstName").val();
     let uLastName=$("#txtLastName").val();
+    let uAge=$("#numAge").val();
+    let uAddress=$("#txtAddress").val();
+    let uPhone=$("#txtPhone").val();
     let uEmail=$("#txtEmail").val();
     let uPass=$("#txtPassword").val();
     let uPayment=$("#txtPayment").val();
     let uColor=$("#txtColor").val();
 
-    user = new User(uFirstName, uLastName, uEmail, uPass, uPayment, uColor);
+    user = new User(uFirstName, uLastName, uAge, uAddress, uPhone, uEmail, uPass, uPayment, uColor);
 
     console.log(user);
 }
